2dfb9ab12af9f4a45ac1d026269bb771710b90de
MatekZ <256511@student.pwr.edu.pl>
2021-12-26 17:11:42 +0100

    funkcjonalnosc like

--- src/posts/urls.py
+++ src/posts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import posts_view
+from .views import posts_view, like_view
 
 app_name = 'posts'
 
 urlpatterns = [
     path('', posts_view, name='main_post_view'),
+    path('liked/', like_view, name='like_post_view'),
 ]
--- src/posts/views.py
+++ src/posts/views.py
@@ -1,11 +1,43 @@
-from django.shortcuts import render
-from .models import Post
+from django.shortcuts import render, redirect
+from .models import Post, Like
+from profiles.models import Profile
+
 
 def posts_view(requset):
     query_set = Post.objects.all()
+    profile = Profile.objects.get(user=requset.user)
 
     context = {
         'qs': query_set,
+        'profile': profile,
     }
 
     return render(requset, 'posts/main.html', context)
+
+
+def like_view(request):
+    user = request.user
+    if request.method == 'POST':
+        post_id = request.POST.get('post_id')
+        post_obj = Post.objects.get(id=post_id)
+        profile = Profile.objects.get(user=user)
+
+        if profile in post_obj.liked.all():
+            post_obj.liked.remove(profile)
+        else:
+            post_obj.liked.add(profile)
+
+        like, created = Like.objects.get_or_create(user=profile, post_id=post_id)
+
+        if not created:
+            if like.value == 'Like':
+                like.value = 'Unlike'
+            else:
+                like.value = 'Like'
+        else:
+            like.value = 'Like'
+
+        post_obj.save()
+        like.save()
+
+    return redirect('posts:main_post_view')
